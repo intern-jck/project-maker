05994b229e24724b22121310c52bdd69bb1bd68c
Justin Kirk <justin.c.kirk@gmail.com>
2024-08-11 18:26:45 -0400

    all crud parts added to photos db

--- server/db/dbPhotos.js
+++ server/db/dbPhotos.js
@@ -2,11 +2,9 @@ const db = require("../db/db.js");
 
 function selectPhotos() {
   const sql = `SELECT * FROM photos;`;
-  // const sql = `SELECT * FROM photos WHERE project_id=${id};`;
 
   return new Promise((resolve, reject) => {
     db.all(sql, (error, rows) => {
-      // console.log(rows);
       if (error) {
         reject(error);
       }
@@ -29,7 +27,6 @@ function selectProjectPhotos(id) {
 }
 
 function insertPhoto(id, data) {
-  console.log("insert photo", id, data);
 
   const sql = `
       INSERT INTO photos (
@@ -58,56 +55,12 @@ function insertPhoto(id, data) {
 }
 
 function updatePhoto(data) {
-  console.log('update photo:\n', data);
-  // return false;
 
   if (!Object.keys(data).length) {
     return false;
   }
 
   const { id, photo_project_id, created_on, url, name } = data;
-
-  /**
-   * 
-   * 
-  
-  FOR:
-  
-  CREATE TABLE Book 
-    ID     INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name   VARCHAR(60) UNIQUE,
-    TypeID INTEGER,
-    Level  INTEGER,
-    Seen   INTEGER
-
-   insert or replace into Book (ID, Name, TypeID, Level, Seen) values
-  ((select ID from Book where Name = "SearchName"), "SearchName", ...);
-
-    INSERT OR REPLACE INTO photos (
-      id,
-      project_id,
-      created_on,
-      url,
-      name
-    )
-    VALUES (
-      (SELECT id FROM photos WHERE id=${id}),
-      {project_id},
-      {created_on},
-      {url},
-      {name}
-    );
-   * 
-   */
-
-  // const sql = `
-  //     UPDATE photos
-  //       SET
-  //       photo_project_id = ${id},
-  //       url = '${data.url}',
-  //       name = '${data.name}'
-  //     WHERE id = ${data.id};
-  //       `;
 
   const sql = `      
     INSERT OR REPLACE INTO photos (
@@ -125,8 +78,6 @@ function updatePhoto(data) {
       '${name}'
     );
       `;
-
-  console.log(sql);
 
   return new Promise((resolve, reject) => {
     db.exec(sql, (error) => {
