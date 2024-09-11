8a26700a0622205af6a0dc46bb266eea531fad1b
Justin Kirk <justin.c.kirk@gmail.com>
2024-09-11 01:26:59 -0400

    moves styles for project

--- client/App.jsx
+++ client/App.jsx
@@ -111,57 +111,6 @@ export default function App() {
           deleteProjectHandler={deleteProjectHandler}
           closeProjectHandler={closeProjectHandler}
         />
-{/* 
-        <div id="app-form">
-          <div id="app-form-header">
-            {Object.keys(currentProject).length ? (
-              <>
-                <span>
-                  ID: {currentProject.id} PROJECT: {currentProject.name}
-                </span>
-                <div>
-                  <button
-                    name="delete-project"
-                    value={currentProject.id}
-                    onClick={deleteProjectHandler}
-                  >
-                    <i className="fa-solid fa-trash-can"></i>
-                  </button>
-                  <button name="close-project" onClick={closeProjectHandler}>
-                    <i className="fa-solid fa-xmark"></i>
-                  </button>
-                </div>
-              </>
-            ) : (
-              <></>
-            )}
-          </div>
-
-          <div id="app-form-content">
-            {Object.keys(currentProject).length ? (
-              <>
-                <InfoForm
-                  name="project-info"
-                  id="project-info"
-                  className="project-info"
-                  infoData={currentProject}
-                  submitForm={getProjectsHandler}
-                />
-                <PhotoForm
-                  projectId={currentProject.id}
-                  projectName={currentProject.name}
-                  name="project-photos"
-                  id="project-photos"
-                  className="project-photos"
-                  photosData={currentProjectPhotos}
-                  submitForm={getProjectsHandler}
-                />
-              </>
-            ) : (
-              <h2>no project selected</h2>
-            )}
-          </div>
-        </div> */}
 
       </div>
     </div>
