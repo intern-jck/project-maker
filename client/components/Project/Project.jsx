d581d68b1824f4e70a48383108b0ef0eccf654f2
Justin Kirk <justin.c.kirk@gmail.com>
2024-09-15 13:51:17 -0400

    project finished

--- client/components/Project/Project.jsx
+++ client/components/Project/Project.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import InfoForm from "../Forms/InfoForm.jsx";
 import PhotoForm from "../Forms/PhotoForm.jsx";
@@ -39,11 +39,11 @@ export default function Project({
               submitForm={onGetProjects}
             />
             <PhotoForm
-              projectId={currentProject.id}
-              projectName={currentProject.name}
               name="project-photos"
               id="project-photos"
               className="project-photos"
+              projectId={currentProject.id}
+              projectName={currentProject.name}
               photosData={currentProjectPhotos}
               submitForm={onGetProjects}
             />
@@ -52,55 +52,6 @@ export default function Project({
       ) : (
         <></>
       )}
-
-      {/* <div className="header">
-        {Object.keys(currentProject).length ? (
-          <>
-            <span>
-              ID: {currentProject.id} PROJECT: {currentProject.name}
-            </span>
-            <div>
-              <button
-                name="delete-project"
-                value={currentProject.id}
-                onClick={onDeleteProject}
-              >
-                <i className="fa-solid fa-trash-can"></i>
-              </button>
-              <button name="close-project" onClick={onCloseProject}>
-                <i className="fa-solid fa-xmark"></i>
-              </button>
-            </div>
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
-
-      <div className="content">
-        {Object.keys(currentProject).length ? (
-          <>
-            <InfoForm
-              name="project-info"
-              id="project-info"
-              className="project-info"
-              infoData={currentProject}
-              submitForm={onGetProjects}
-            />
-            <PhotoForm
-              projectId={currentProject.id}
-              projectName={currentProject.name}
-              name="project-photos"
-              id="project-photos"
-              className="project-photos"
-              photosData={currentProjectPhotos}
-              submitForm={onGetProjects}
-            />
-          </>
-        ) : (
-          <h2>no project selected</h2>
-        )}
-      </div> */}
     </div>
   );
 }
